a1f217103ac245f9b2066afe2ade77e6a7e63d92
simlexx-k <simonwriter254@gmail.com>
2024-02-11 22:42:04 +0300

    edited navigation items to navigation-menu.blade.php

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,31 +1,9 @@
 <x-app-layout>
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/main
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Registration') }}
         </h2>
     </x-slot>
 
-<<<<<<< HEAD
-=======
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
-        </div>
-    </div>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
-                <h2 class="text-lg font-semibold mb-4">Register Farmer</h2>
-                <livewire:register-farmer />
-            </div>
-        </div>
-    </div>
->>>>>>> origin/main
 </x-app-layout>
