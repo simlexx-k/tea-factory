27fa39f8f04217fe36c3d4e89175b280c5327f8d
simlexx-k <simonwriter254@gmail.com>
2024-02-12 08:54:42 +0300

    edited navigation items to navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,7 +16,7 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
@@ -48,22 +48,121 @@
                         </x-slot>
                     </x-dropdown>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('records') }}" :active="request()->routeIs('records')">
-                        {{ __('Records') }}
-                    </x-nav-link>
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                {{ __('Records') }}
+                                <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <!-- Records Management -->
+                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                {{ __('Manage Records') }}
+                            </div>
+
+                            <!-- Record Daily Deliveries -->
+                            <x-dropdown-link href="{{ route('records') }}">
+                                {{ __('Record Daily Deliveries') }}
+                            </x-dropdown-link>
+
+                            <!-- View Recorded Deliveries -->
+                            <x-dropdown-link href="{{ route('records') }}">
+                                {{ __('View Recorded Deliveries') }}
+                            </x-dropdown-link>
+
+                            <!-- Manage Records -->
+                            <x-dropdown-link href="{{ route('records') }}">
+                                {{ __('Manage Records') }}
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
                 </div>
 
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('payments') }}" :active="request()->routeIs('payments')">
-                        {{ __('Payments') }}
-                    </x-nav-link>
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                {{ __('Payments') }}
+                                <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <!-- Records Management -->
+                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                {{ __('Manage Payments') }}
+                            </div>
+
+                            <!-- Payrolls -->
+                            <x-dropdown-link href="{{ route('records') }}">
+                                {{ __('Create a Payroll') }}
+                            </x-dropdown-link>
+
+                            <!-- View Previous Payrolls -->
+                            <x-dropdown-link href="{{ route('records') }}">
+                                {{ __('View Finalised Payrolls') }}
+                            </x-dropdown-link>
+
+                            <!-- Manage Deductions -->
+                            <x-dropdown-link href="{{ route('records') }}">
+                                {{ __('Manage Deductions') }}
+                            </x-dropdown-link>
+
+                            <!-- Manage Payrates -->
+                            <x-dropdown-link href="{{ route('records') }}">
+                                {{ __('Manage Pay Rates') }}
+                            </x-dropdown-link>
+
+                        </x-slot>
+                    </x-dropdown>
                 </div>
 
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('reports') }}" :active="request()->routeIs('reports')">
-                        {{ __('Reports') }}
-                    </x-nav-link>
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                {{ __('Reports') }}
+                                <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <!-- Reports -->
+                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                {{ __('Generate Reports') }}
+                            </div>
+
+                            <!-- Payrolls Reports-->
+                            <x-dropdown-link href="{{ route('reports') }}">
+                                {{ __('Generate Payroll Reports') }}
+                            </x-dropdown-link>
+
+                            <!-- Manage Payrates -->
+                            <x-dropdown-link href="{{ route('reports') }}">
+                                {{ __('Manage Pay Rates Reports') }}
+                            </x-dropdown-link>
+
+                            <!-- Generate Tea Delivery Records -->
+                            <x-dropdown-link href="{{ route('reports') }}">
+                                {{ __('Generate Tea Delivery Records') }}
+                            </x-dropdown-link>
+
+                            <!-- Manage Another Thing -->
+                            <x-dropdown-link href="{{ route('reports') }}">
+                                {{ __('Another Thing') }}
+                            </x-dropdown-link>
+
+                        </x-slot>
+                    </x-dropdown>
                 </div>
 
 
